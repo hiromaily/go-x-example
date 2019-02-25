4a4e31d1ad9b4a346c2a84af247431429fa6107a
hiroki-money <yasui.hiroki@mf-financial.jp>
2019-02-25 15:07:50 +0900

    added bcrypt

---
+++ crypto/bcrypt/bcrypt_test.go
@@ -0,0 +1,31 @@
+package hash_test
+
+import (
+	"fmt"
+	"testing"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestBcrypt(t *testing.T) {
+	userPassword1 := "some user-provided password"
+
+	// Generate "hash" to store from user password
+	hash, err := bcrypt.GenerateFromPassword([]byte(userPassword1), bcrypt.DefaultCost)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("Hash to store:", string(hash))
+	// Store this "hash" somewhere, e.g. in your database
+
+	// After a while, the user wants to log in and you need to check the password he entered
+	userPassword2 := "some user-provided password"
+	hashFromDatabase := hash
+
+	// Comparing the password with the hash
+	if err := bcrypt.CompareHashAndPassword(hashFromDatabase, []byte(userPassword2)); err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("Password was correct!")
+}
