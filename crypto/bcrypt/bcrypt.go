151a78898a5951cbef61b5f954462a916076d125
hiroki-money <yasui.hiroki@mf-financial.jp>
2019-02-26 15:58:44 +0900

    tweak

--- crypto/bcrypt/bcrypt.go
+++ crypto/bcrypt/bcrypt.go
@@ -1,1 +1,4 @@
-package hash
+package bcrypt
+
+// Package bcrypt implements Provos and Mazières's bcrypt adaptive hashing algorithm
+// https://godoc.org/golang.org/x/crypto/bcrypt
--- crypto/bcrypt/bcrypt_test.go
+++ crypto/bcrypt/bcrypt_test.go
@@ -1,4 +1,4 @@
-package hash_test
+package bcrypt_test
 
 import (
 	"fmt"
